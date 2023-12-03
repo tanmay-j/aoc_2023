28ee7e73042d0cdb5aed2d4b779b9a7b785bb9f1
tanmay-j <tanmay18271@iiitd.ac.in>
2023-12-03 05:49:06 +0000

    Built initial branching

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::io;
+
+fn first() {
+    let input_filename = "data/1.txt";
+    println!("First");
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Which day do you want solved?");
+
+    let mut day = String::new();
+
+    io::stdin()
+        .read_line(&mut day)
+        .expect("Failed to read line");
+
+    let day:u32 = day.trim().parse().expect("Not a valid number!");
+
+    println!("You chose day {day}");
+
+    match day{
+        1=>first(),
+        _=>println!("Unsolved"),
+    }
 }
