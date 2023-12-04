d6867b9126f9623e1ed2e0a9d3ea4730028d7c4e
tanmay-j <tanmay18271@iiitd.ac.in>
2023-12-04 18:49:05 +0000

    Finalized Day 1 silver, started gold

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # aoc_2023
 Advent of Code 2023: Rust
+
+### My Current Progress
+
+| Day   | Silver | Gold       |
+|-------|:-----:|------------:|
+| Day 1 |  Done | In Progress |
+| Day 2 |       |             |
+
+### What I would like to learn next
+- Array Data types and how to access elements safely 
+
+### Links of good sources
+- [Official Rust Book](https://doc.rust-lang.org/book/title-page.html)
+- [What is the difference between String and &str in Rust?]( https://stackoverflow.com/questions/24158114/what-are-the-differences-between-rusts-string-and-str)
--- src/first.rs
+++ src/first.rs
@@ -3,12 +3,18 @@ use std::fs;
 pub fn run() {
     let input_filepath = "src/data/1.txt";
     println!("First");
-    unoptimized_silver(input_filepath)
+
+    let silver_answer =  unoptimized_silver(input_filepath);
+    println!("Silver Solution: {silver_answer}");
+
+    // Gold not working yet
+    
+    // let gold_answer =  unoptimized_gold(input_filepath);
+    // println!("Gold Solution: {gold_answer}"); 
 }
 
 
-fn unoptimized_silver(filepath: &str){
-    println!("In file {}", filepath);
+fn unoptimized_silver(filepath: &str) -> u32{
 
     let contents = fs::read_to_string(filepath)
         .expect("Should have been able to read the file");
@@ -21,13 +27,10 @@ fn unoptimized_silver(filepath: &str){
 
 
     for line in lines {
-        // println!("Lines: {line}");
         let mut first:u32 = 0;
         let mut last:u32 = 0;
         for c in line.chars() {
-            // print!("{c}");
             if c >= '0' && c <= '9'{
-                // println!("habibi! {c}");
                 first = c as u32;
                 first -= zero;
                 break;
@@ -36,7 +39,6 @@ fn unoptimized_silver(filepath: &str){
 
         for c in line.chars().rev() {
             if c >= '0' && c <= '9'{
-                // println!("ibibah! {c}");
                 last = c as u32;
                 last -= zero;
                 break;
@@ -45,10 +47,69 @@ fn unoptimized_silver(filepath: &str){
         let result:u32 = first*10 + last;
         
         sum+= result;
-        // println!("{result}");
     }
 
-    println!("Result: {sum}")
-
-    // println!("With text:\n{contents}");
+    return sum;
 }
+
+// ##################################################################################
+// The code below needs fixing 
+// ##################################################################################
+
+// fn advance_state_machine(string: &str, cur: char, state: [usize]) -> usize {
+//     if string.chars().nth(state).expect("Index out of bounds!")==cur {
+//         return state+1;
+//     }
+//     else{
+//         return 0;
+//     }
+// }
+
+// fn unoptimized_gold(filepath: &str) -> u32{
+//     let strs = ["zero","one","two","three","four","five","six","seven","eight","nine"];
+    
+//     let contents = fs::read_to_string(filepath)
+//     .expect("Should have been able to read the file");
+    
+//     let mut sum: u32 = 0;
+    
+//     let lines = contents.lines();
+    
+//     let zero:u32 = '0' as u32;
+    
+    
+//     for line in lines {
+//         let mut first:u32 = 0;
+//         let mut last:u32 = 0;
+
+//         let mut states: [usize; 10] = [0; 10] ;
+//         for c in line.chars() {
+//             if c >= '0' && c <= '9'{
+//                 first = c as u32;
+//                 first -= zero;
+//                 break;
+//             }
+//             for i in [0..10] {
+//                 states[i] = advance_state_machine(strs[i], c, states[i]);
+//                 if states[i] == strs[i].len() {
+//                     first = i;
+//                     break;
+//                 }
+//             }
+//         }
+
+//         for c in line.chars().rev() {
+//             if c >= '0' && c <= '9'{
+//                 last = c as u32;
+//                 last -= zero;
+//                 break;
+//             }
+//         }
+//         let result:u32 = first*10 + last;
+        
+//         sum+= result;
+//     }
+
+//     return sum;
+
+// }
